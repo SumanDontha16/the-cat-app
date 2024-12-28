cb918b073081ccf139160f4bdcb0b63721f8c335
Dontha Suman <51446173+SumanDontha16@users.noreply.github.com>
2024-12-28 15:34:24 +0000

    Added the Upload component with logic.

--- src/pages/Upload/Upload.jsx
+++ src/pages/Upload/Upload.jsx
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
+import { useUploadCatImageMutation } from '../../api/catApi';
+import { useDispatch } from 'react-redux';
+import { addCatImage } from '../../features/catSlice';
 import { Link } from 'react-router-dom';
 
 export const Upload = () => {
+    const [ file, setFile ] = useState(null);
+    const [ isUploading, setIsUploading ] = useState(false);
+    const [ uploadCatImage ] = useUploadCatImageMutation();
+    const navigate = useNavigate();
+    const dispatch = useDispatch();
+
+    const handleFileChange = (e) => {
+        e.preventDefault();
+        const selectedFile = e.target.files[0];
+
+        // Validate file type
+        if (selectedFile && (selectedFile.type === 'image/jpeg' || selectedFile.type === 'image/png')) {
+            setFile(selectedFile);
+        } else {
+            // Reset file if type is invalid
+            setFile(null);
+            alert('Please upload a valid image file (.jpg or .png)'); // You can customize this as needed
+        }
+    };
+
+    const handleUpload = async (e) => {
+        e.preventDefault();
+        if (file) {
+            const formData = new FormData();
+            formData.append('file', file);
+            setIsUploading(true);
+
+            try {
+                const response = await uploadCatImage(formData);
+                if (response.data) {
+                    dispatch(addCatImage(response.data.image));
+                    // Redirect to home page after successful upload
+                    navigate('/');
+                } else {
+                    // Handle upload failure silently
+                }
+            } catch (error) {
+                // Handle error silently
+            } finally {
+                setIsUploading(false); // Reset loading state
+            }
+        }
+    };
+
     return (
-        <div>
+        <div data-testid="upload">
             <NavBar />
             <main>
                 <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
@@ -17,6 +65,16 @@ export const Upload = () => {
                         </Link>{' '}
                         or face deletion.
                     </p>
+                    <div className="mt-4 border-t-[1px] border-gray-200 py-6">
+                        <input id="file-upload" type="file" accept="image/*" onChange={handleFileChange} />
+                        <button
+                            onClick={handleUpload}
+                            className="mt-4 bg-indigo-500 text-white py-2 px-4 rounded-md hover:bg-indigo-600"
+                            disabled={isUploading || !file} // Disable the button during upload and if no file is selected
+                        >
+                            {isUploading ? 'Uploading...' : 'Upload'}
+                        </button>
+                    </div>
                 </div>
             </main>
         </div>
