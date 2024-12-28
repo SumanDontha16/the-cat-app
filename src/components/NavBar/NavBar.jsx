d9bb602c5a43b433bde65ed68dccc0692fd2e929
Dontha Suman <51446173+SumanDontha16@users.noreply.github.com>
2024-12-28 16:19:34 +0000

    Added the application title.

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -27,11 +27,7 @@ const NavBar = () => {
                     </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                         <div className="flex shrink-0 items-center">
-                            <img
-                                alt="Your Company"
-                                src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=500"
-                                className="h-8 w-auto"
-                            />
+                            <span className="text-white text-2xl font-bold">The Cat App</span>
                         </div>
                         <div className="hidden sm:ml-6 sm:block">
                             <div className="flex space-x-4">
