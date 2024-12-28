c369fd73a6fb5c11ccb5f94dde0d91ce3e523954
Dontha Suman <51446173+SumanDontha16@users.noreply.github.com>
2024-12-28 15:27:11 +0000

    Added CatCard and CatGallery components logic to the homepage.

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import NavBar from '../../components/NavBar/NavBar';
+import CatGallery from '../../components/CatGallery/CatGallery';
 
 export const HomePage = () => {
     return (
         <div>
             <NavBar />
             <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-6">
-                <h1>Home Page</h1>
+                <CatGallery />
             </main>
         </div>
     );
